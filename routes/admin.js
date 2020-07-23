78b0a07cc51f8b5be8ffbe3b6550dd19b3a2deca
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-07-24 00:32:49 +0300

    feature(Add View Doctors Page): Viewing Doctor Details

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const bcrypt  = require('bcrypt');
+const bcrypt  = require('bcrypt');
 const router  = express.Router();
 const User    = require('../models/users');
 const Doctor  = require('../models/doctors');
@@ -7,7 +7,7 @@ const Patient = require('../models/patients');
 
 const rounds  = 12;
 
-const { body,validationResult,sanitizeBody } = require('express-validator');
+const { body,check, validationResult, matchedData,sanitizeBody } = require('express-validator');
 
 /******************
   
@@ -38,14 +38,45 @@ router.get('/admin-doctors',(req,res)=>{
 });
 
 router.get('/admin-create-doctor',(req,res)=>{
-  res.render('admin/admin-create-doctor');
+  res.render('admin/admin-create-doctor',{ data: {}, errors: {} });
 });
 
-router.post('/admin-doctor/register',(req,res)=>{
+router.post('/admin-doctor/register',[
+    check("names")
+      .isLength({ min: 1 })
+      .withMessage("Names is required")
+      .trim(),
+    check("license_number")
+      .isLength({ min: 1 })
+      .withMessage("License Number is required")
+      .trim(),
+    check("hospital")
+      .isLength({ min: 1 })
+      .withMessage("Hospital is required")
+      .trim(),
+    check("speciality")
+      .isLength({ min: 1 })
+      .withMessage("Speciality is required")
+      .trim(),
+    check("password")
+      .isLength({ min: 1 })
+      .withMessage("Password is required")
+      .trim(),
+    check("email")
+      .isEmail()
+      .withMessage("Provide a valid email address")
+      .bail()
+      .trim()
+      .normalizeEmail()
+  ],(req,res)=>{
   
-  bcrypt.hash(req.body.password, rounds, function(err,hash) {
-    if(err){
-      console.log(err);
+  bcrypt.hash(req.body.password, rounds, function(err,hash){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.render("admin/admin-create-doctor", {
+        data: req.body,
+        errors: errors.mapped()
+      });
     }else{
       let doctor = new Doctor({
         names: req.body.names,
@@ -55,6 +86,7 @@ router.post('/admin-doctor/register',(req,res)=>{
         email: req.body.email,
         password: hash
       }).save();
+      req.flash("success", "Doctor successfully registered.");
       res.redirect('/admin/admin-doctors');
     }
   });
@@ -62,8 +94,33 @@ router.post('/admin-doctor/register',(req,res)=>{
 });
 
 router.get('/admin-doctor-details/:id',(req,res)=>{
-  res.render('admin/admin-doctor-details');
+
+  Doctor.findById(req.params.id, (error, doctor)=>{
+     if(error){
+        console.log(error)
+     }else{
+       let patients=20;
+       let prescriptions=10;
+       const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+       res.render('admin/admin-doctor-details', {doctor:doctor, months: months,
+        patients: patients, prescriptions: prescriptions});
+     }
+   });
+
 });
+
+router.delete('/admin-doctor/:id', (req,res)=>{
+  
+   Doctor.deleteOne({_id:req.params.id},(error)=>{
+     if(error){
+      console.log(err);
+      res.send('failed');
+     }else{
+      res.send('success');
+    }
+   });
+
+})
 
 /********************
   
