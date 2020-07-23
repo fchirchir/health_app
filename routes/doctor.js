f7c83af16bd7c8e96fa28bb85baa3ea1e972a93e
Maureen Sitienei <mstienei@gmail.com>
2020-07-23 11:22:51 -0400

    ft: add navigation between doctor homepage and consultation view

--- routes/doctor.js
+++ routes/doctor.js
@@ -8,10 +8,8 @@ router.get('/',(req,res)=>{
     res.render('doctor/doctor-consultations');
   });
   
-  router.get('/doctor-view-consultations',(req,res)=>{
+  router.get('/doctor-view-consultation',(req,res)=>{
     res.render('doctor/doctor-view-consultation');
   });
-
-
 
 module.exports = router;
