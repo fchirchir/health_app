5c4d2655cacddeae04e6797bfff41dc071c46a2b
Maureen Sitienei <mstienei@gmail.com>
2020-07-24 06:25:33 -0400

    ft:integrate doctor consultation

--- routes/doctor.js
+++ routes/doctor.js
@@ -2,14 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 const Doctor = require('../models/doctors');
+const Patient = require('../models/patients');
 
+ 
+//doctor routes
+  router.get('/',(req,res)=>{ 
+    res.render('doctor/doctor-profile'); 
+ });
 
-router.get('/',(req,res)=>{
-    res.render('doctor/doctor-consultations');
-  });
   
   router.get('/doctor-view-consultation',(req,res)=>{
     res.render('doctor/doctor-view-consultation');
   });
+
+  router.get('/doctor-consultation',(req,res)=>{
+    //search patients
+  Patient.find({}, (err, patients)=>{
+    if(err){
+       console.log(err);
+    }else{
+       res.render('doctor/doctor-consultations', {patients:patients});
+    }
+  });
+  });
+
 
 module.exports = router;
